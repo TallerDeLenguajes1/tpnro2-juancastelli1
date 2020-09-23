b9a753b82856da85b6fb7c4a2c68bfb7fc246362
juancastelli1 <juancastelli12345@gmail.com>
2020-09-23 14:08:21 -0300

    TP2 Completo

--- TP02/Problema4/Empleado.cs
+++ TP02/Problema4/Empleado.cs
@@ -6,54 +6,86 @@ using System.Threading.Tasks;
 
 namespace Problema4
 {
+    public enum estadociv { Casado, Soltero, Divorciado };
+    public enum tienetitulo { Si, No, SinEspecificar };
+    public struct universidad
+    {
+        public tienetitulo contitulo;
+        public string titulo;
+        public string nombreuni;
+    }
+    public struct direccion
+    {
+        public string calle;
+        public int numero;
+    }
     class Empleado
     {
+        //miembros
         int DNI;
         string nombre;
         string apellido;
         DateTime fechanac;
-        struct direccion 
-        {
-            string calle;
-            int numero;
-        }
+        direccion direc;
         DateTime fechaingreso;
+        universidad datosuni;
         string profesion;
-        enum estadociv {Casado,Soltero,Divorciado};
+        estadociv estcivil;
         int canthijos;
-        struct universidad 
+        public Empleado(int DNIE, string nombreE, string apellidoE, DateTime fechanacE, 
+            direccion direcE, DateTime fechaingresoE, universidad datosuniE, string profesionE,
+            estadociv estcivilE, int canthijosE)
         {
-            enum tienetitulo {Si,No};
-            string titulo;
-            string nombreuni;
+            DNI1 = DNIE;
+            Nombre = nombreE;
+            Apellido = apellidoE;
+            Fechanac = fechanacE;
+            Direc = direcE;
+            Fechaingreso = fechaingresoE;
+            Datosuni = datosuniE;
+            Profesion = profesionE;
+            Estcivil = estcivilE;
+            Canthijos = canthijosE;
         }
-        int Antiguedad() 
+
+        public int DNI1 { get => DNI; set => DNI = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime Fechanac { get => fechanac; set => fechanac = value; }
+        public direccion Direc { get => direc; set => direc = value; }
+        public DateTime Fechaingreso { get => fechaingreso; set => fechaingreso = value; }
+        public universidad Datosuni { get => datosuni; set => datosuni = value; }
+        public string Profesion { get => profesion; set => profesion = value; }
+        public estadociv Estcivil { get => estcivil; set => estcivil = value; }
+        public int Canthijos { get => canthijos; set => canthijos = value; }
+
+        public int Antiguedad() 
         {
             DateTime zeroTime = new DateTime(1, 1, 1);
             DateTime today = DateTime.Today;
-            TimeSpan years = today - fechaingreso;
+            TimeSpan years = today - Fechaingreso;
             return (zeroTime + years).Year - 1;
         }
-        int Edad()
+        public int Edad()
         {
             DateTime fechaActual = DateTime.Today;
-            if (fechanac > fechaActual)
+            if (Fechanac > fechaActual)
             {
                 Console.WriteLine("La fecha de nacimiento es mayor que la actual.");
                 return -1;
             }
             else
             {
-                int edad = fechaActual.Year - fechanac.Year;
-                if (fechanac.Month > fechaActual.Month)
+                int edad = fechaActual.Year - Fechanac.Year;
+                if (Fechanac.Month > fechaActual.Month)
                 {
                     --edad;
                 }
                 else
                 {
-                    if (fechanac.Month == fechaActual.Month)
+                    if (Fechanac.Month == fechaActual.Month)
                     {
-                        if (fechanac.Day > fechaActual.Day)
+                        if (Fechanac.Day > fechaActual.Day)
                         {
                             --edad;
                         }
@@ -61,6 +93,22 @@ namespace Problema4
                 }
                 return edad;
             }
+        }
+        public double salario() 
+        {
+            //Tomo sueldo basico como 50000
+            double sueldobasic = 50000;
+            double descuento = sueldobasic * 0.15;
+            double adicional;
+            if (this.Antiguedad() > 20)
+            {
+                adicional = sueldobasic * 0.25;
+            }
+            else 
+            {
+                adicional = sueldobasic * (this.Antiguedad() / 10);
+            }
+            return (sueldobasic + adicional - descuento);
         }
     }
 }
--- TP02/Problema4/Program.cs
+++ TP02/Problema4/Program.cs
@@ -3,13 +3,105 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Problema4
 {
     class Program
     {
+        private static readonly Logger loggeador = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            List < Empleado > ListaEmpleados = new List<Empleado>();
+            int n;
+            Console.Write("Cuantos trabajadores quiere cargar?");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++) 
+            {
+                try
+                {
+                    Console.Write("Ingrese DNI empleado:");
+                    int DNIE = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese nombre empleado:");
+                    string nombreE = Console.ReadLine();
+                    Console.Write("Ingrese apellido empleado:");
+                    string apellidoE = Console.ReadLine();
+                    Console.Write("Ingrese fecha de nacimiento empleado:");
+                    DateTime fechanacE = DateTime.Parse(Console.ReadLine());
+                    direccion direcE;
+                    Console.Write("Ingrese calle del empleado:");
+                    direcE.calle = Console.ReadLine();
+                    Console.Write("Ingrese numero de casa del empleado:");
+                    direcE.numero = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese fecha de ingreso empleado:");
+                    DateTime fechaingresoE = DateTime.Parse(Console.ReadLine());
+                    universidad datosuniE;
+                    Console.Write("El empleado tiene titulo?(Si, No):");
+                    switch (Console.ReadLine())
+                    {
+                        case "Si":
+                            datosuniE.contitulo = tienetitulo.Si;
+                            break;
+                        case "No":
+                            datosuniE.contitulo = tienetitulo.No;
+                            break;
+                        default:
+                            datosuniE.contitulo = tienetitulo.SinEspecificar;
+                            break;
+                    }
+                    Console.Write("Que Titulo Tiene?:");
+                    datosuniE.titulo = Console.ReadLine();
+                    Console.Write("Donde Lo Obtuvo?:");
+                    datosuniE.nombreuni = Console.ReadLine();
+                    Console.Write("Profesion Del Empleado?:");
+                    string profesionE = Console.ReadLine();
+                    estadociv estcivilE;
+                    Console.Write("Elija estado civil:\n1. Soltero\n2. Casado\n3. Divorciado");
+                    switch (int.Parse(Console.ReadLine()))
+                    {
+                        case 1:
+                            estcivilE = estadociv.Soltero;
+                            break;
+                        case 2:
+                            estcivilE = estadociv.Casado;
+                            break;
+                        case 3:
+                            estcivilE = estadociv.Divorciado;
+                            break;
+                        default:
+                            estcivilE = estadociv.Soltero;
+                            break;
+                    }
+                    Console.Write("Ingrese Cantidad De Hijos:");
+                    int canthijosE = int.Parse(Console.ReadLine());
+                    ListaEmpleados.Add(new Empleado(DNIE, nombreE, apellidoE, fechanacE, direcE,
+                        fechaingresoE, datosuniE, profesionE, estcivilE, canthijosE));
+                }
+                catch (System.FormatException ex)
+                {
+                    Console.WriteLine("Error: Se intento cargar otro formato de alguna variable");
+                    loggeador.Fatal("Se intento cargar otro formato de alguna variable");
+                    loggeador.Fatal(ex.ToString());
+                }
+                catch (Exception ex2)
+                {
+                    Console.WriteLine("Error: Error desconocido");
+                    loggeador.Fatal("Error desconocido");
+                    loggeador.Fatal(ex2.ToString());
+                }
+            }
+            foreach (Empleado emp in ListaEmpleados) 
+            {
+                Console.Write("Empleado de DNI:" + emp.DNI1 + "\n");
+                Console.Write("Nombre:" + emp.Nombre + "\n");
+                Console.Write("Apellido:" + emp.Apellido + "\n");
+                Console.Write("Edad:" + emp.Edad() + "\n");
+                Console.Write("Antiguedad:" + emp.Antiguedad() + "\n");
+                Console.Write("Salario:" + emp.salario() + "\n");
+                Console.Write("\n");
+                Console.Write("\n");
+            }
+            Console.ReadLine();
         }
     }
 }
